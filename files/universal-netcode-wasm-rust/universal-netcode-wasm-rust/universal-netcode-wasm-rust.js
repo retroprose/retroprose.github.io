04c004f39cf785e147e71047ac27dd27a844235c
Jason <eatonj@umich.edu>
2023-02-08 10:08:50 -0500

    test wasm

---
+++ files/universal-netcode-wasm-rust/universal-netcode-wasm-rust/universal-netcode-wasm-rust.js
@@ -0,0 +1,6 @@
+import * as wasm from "./universal-netcode-wasm-rust_bg.wasm";
+import { __wbg_set_wasm } from "./universal-netcode-wasm-rust_bg.js";
+__wbg_set_wasm(wasm);
+export * from "./universal-netcode-wasm-rust_bg.js";
+
+wasm.__wbindgen_start();
