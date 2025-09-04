9477dc7dff690ca43ba44f44cc155703a5d8f096
eatjason <retroprose11@gmail.com>
2025-09-04 18:45:35 -0400

    added map

--- files/celestial-creepers/scripts/synced-states/bomber.js
+++ files/celestial-creepers/scripts/synced-states/bomber.js
@@ -53,7 +53,7 @@ class BomberIO extends PIXI.Container {
         // round: return floor(d + 0.5);
         // output = output_start + round(slope * (input - input_start));
         //let slope = 1.0 * (32767 - -32768) / (1.0 - -1.0);
-        let slope = 1.0 * (32767 - -32767) / (1.0 - -1.0);
+        let slope = 1.0 * (32767 - -32767) / (32767);
 
         /*for (const gamepad of navigator.getGamepads()) {
             if (!gamepad) continue;
@@ -73,8 +73,16 @@ class BomberIO extends PIXI.Container {
             if (gamepad.buttons[0].pressed) this.bindInput.detonate = true;
         }*/
 
-        this.bindInput.x = -32767 + slope * (this.input.stickStateX - -1.0);
-        this.bindInput.y = -32767 + slope * (this.input.stickStateY - -1.0);
+        this.bindInput.x = 0;
+        this.bindInput.y = 0;
+
+        /*if (this.input.keyState['w']) this.bindInput.y = -32767;
+        if (this.input.keyState['s']) this.bindInput.y = 32767;
+        if (this.input.keyState['a']) this.bindInput.x = -32767;
+        if (this.input.keyState['d']) this.bindInput.x = 32767;*/
+
+        this.bindInput.x = -32767 + slope * (this.input.left);
+        this.bindInput.y = -32767 + slope * (this.input.right);
 
         if (this.buttonState == 1) this.bindInput.bomb = true;
 
@@ -92,6 +100,34 @@ class BomberIO extends PIXI.Container {
         this.screen.begin();
 
         let entity = this.entity;
+
+        let sx = Math.floor(-this.screen.container.x / 16);
+        let sy = Math.floor(-this.screen.container.y / 16);
+        let ex = sx + Math.floor(960 / 16) + 1;
+        let ey = sy + Math.floor(540 / 16) + 1;
+
+        if (sx < 0) sx = 0;
+        if (sy < 0) sy = 0;
+        if (ex >= 512) ex = 511;
+        if (ey >= 512) ey = 511;
+
+        // render the tile map
+        for (let row = sy; row <= ey; ++row)
+        {
+            for (let col = sx; col <= ex; ++col)
+            { 
+                const t = this.game.getTile(col, row);
+                if (t == 1) {
+                    let s = this.screen.next();
+                    s.x = col * 16;
+                    s.y = row * 16;
+                    s.texture = this.pixel;
+                    s.tint = 0xc8c8c8;
+                    s.width = 16;
+                    s.height = 16;
+                }
+            }
+        }
 
         this.game.findLocal(this.local);
         entity.begin(this.game);
