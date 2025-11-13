1662d7d79f9419c2bbc5d136123cc57715ab3612
eatjason <retroprose11@gmail.com>
2025-11-13 15:52:52 -0500

    updated physics

--- files/sport/scripts/synced-states/sport.js
+++ files/sport/scripts/synced-states/sport.js
@@ -41,6 +41,7 @@ class SportIO extends PIXI.Container {
 
     processInput(buffer) {
 
+        // clear out all the booleans
         this.bindInput.up = false;
         this.bindInput.down = false;
         this.bindInput.left = false;
@@ -50,42 +51,15 @@ class SportIO extends PIXI.Container {
         this.bindInput.kick = false;
         this.bindInput.detonate = false;
 
-        // int16_t range: -32,768 to 32,767
-        // let slope = 1.0 * (output_end - output_start) / (input_end - input_start);
-        // round: return floor(d + 0.5);
-        // output = output_start + round(slope * (input - input_start));
-        //let slope = 1.0 * (32767 - -32768) / (1.0 - -1.0);
-        let slope = 1.0 * (32767 - -32767) / (1.0 - -1.0);
-
-        /*for (const gamepad of navigator.getGamepads()) {
-            if (!gamepad) continue;
-            if (gamepad.buttons.length < 16) continue;
-
-            this.bindInput.x = -32767 + slope * (gamepad.axes[0] - -1.0);
-            this.bindInput.y = -32767 + slope * (gamepad.axes[1] - -1.0);
-
-            if (gamepad.buttons[12].pressed) this.bindInput.up = true;
-            if (gamepad.buttons[13].pressed) this.bindInput.down = true;
-            if (gamepad.buttons[14].pressed) this.bindInput.left = true;
-            if (gamepad.buttons[15].pressed) this.bindInput.right = true;
-
-            if (gamepad.buttons[1].pressed) this.bindInput.bomb = true;
-            if (gamepad.buttons[2].pressed) this.bindInput.punch = true;
-            if (gamepad.buttons[3].pressed) this.bindInput.kick = true;
-            if (gamepad.buttons[0].pressed) this.bindInput.detonate = true;
-        }*/
-
-        //this.bindInput.x = -32767 + slope * (this.input.stickStateX - -1.0);
-        //this.bindInput.y = -32767 + slope * (this.input.stickStateY - -1.0);
-
+        // find the global target coordinate with respect to the whole play field
         this.targetX = ((this.input.xPos - this.screen.x) / this.screen.finalScale) + this.game.getCameraX();
         this.targetY = ((this.input.yPos - this.screen.y) / this.screen.finalScale) + this.game.getCameraY();
-
+        
+        // set the network input to that target coordinate
         this.bindInput.x = Math.floor(this.targetX);
         this.bindInput.y = Math.floor(this.targetY);
 
-        //console.log("x: " + this.input.xPos + ", " + "y: " + this.input.yPos);
-
+        // I'm using the bomb boolean for if the player has a finger down
         if (this.input.touchDown == true) this.bindInput.bomb = true;
        
         this.game.getInput(this.bindInput, buffer);
@@ -101,7 +75,6 @@ class SportIO extends PIXI.Container {
         this.screen.begin();
 
         // scroll screen to camera position
-        //this.screen.scroll(-this.game.getCameraX() + 270, -this.game.getCameraY() + 480);
         this.screen.scroll(-this.game.getCameraX(), -this.game.getCameraY());
 
         this.game.begin();
@@ -119,6 +92,7 @@ class SportIO extends PIXI.Container {
             s.rotation = this.game.rotation;
         }
 
+        // for debugging
         //this.screen.text(this.game.getCameraX(), this.game.getCameraY(), "" + this.xOffset + ", " + this.yOffset);
         //this.screen.text(this.game.getCameraX(), this.game.getCameraY(), "" + this.input.testX + ", " + this.input.testY);
         //this.screen.text(this.game.getCameraX(), this.game.getCameraY(), "" + this.targetX + ", " + this.targetY);
