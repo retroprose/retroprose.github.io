df208ef928239d94e94813857b192e4877efada9
eatjason <retroprose11@gmail.com>
2025-11-16 10:11:00 -0500

    added one on one sport version

---
+++ files/sport-one/scripts/core/sportinput.js
@@ -0,0 +1,32 @@
+class SportInput extends BasicInput {
+
+    constructor() {
+        super();
+        
+        this.touchDown = false;
+        this.xPos = 0.0;
+        this.yPos = 0.0;
+
+        window.onpointerdown = (e) => {
+            this.touchDown = true;
+            this.xPos = e.clientX;
+            this.yPos = e.clientY;
+        };
+        window.onpointerup = (e) => {
+            this.touchDown = false;
+        };
+        window.onpointermove = (e) => {
+            this.xPos = e.clientX;
+            this.yPos = e.clientY;
+        };
+    }
+
+    resize() {
+
+    }
+
+    update(delta) {
+    
+    }
+    
+}
